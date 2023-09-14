366d8c337e8ae22125d8db3b71bf683c049b3abe
neogiciel <123723616+neogiciel@users.noreply.github.com>
2023-09-14 18:49:20 +0200

    first commit

---
+++ hello-world/src/main/java/org/neogiciel/HelloWorld.java
@@ -0,0 +1,38 @@
+package org.neogiciel;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/")
+public class HelloWorld {
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello";
+    }
+
+    @GET
+    @Path("/test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "Test";
+    }
+
+
+    @GET
+    @Path("get/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String get(@PathParam("id") String id) {
+        
+        return "id = "+ id;
+
+    }
+
+
+}
